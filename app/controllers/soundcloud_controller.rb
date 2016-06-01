623be2b1da40ee8711c7b313b37b788c192ff46a
owen flood <oneflood@owens-MacBook-Pro.local>
2016-06-01 10:57:00 -0700

    Convert song rendering and searching to react-rails

--- app/controllers/soundcloud_controller.rb
+++ app/controllers/soundcloud_controller.rb
@@ -1,8 +1,10 @@
 class SoundcloudController < ApplicationController
   def index
+    client = SoundCloud.new(:client_id => "5d3aaf910add018f35ba65e325fcf227")
+    @tracks = client.get('/tracks', :limit => 10, :order => 'hotness')
   end
 
   def player
-    
+
   end
 end
