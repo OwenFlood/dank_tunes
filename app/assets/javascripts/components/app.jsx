740fbc558b0c153c7cea797f4e21912966589c49
Owen Flood <owenhflood@gmail.com>
2016-06-01 16:18:21 -0700

    add soundcloud parser

--- app/assets/javascripts/components/app.jsx
+++ app/assets/javascripts/components/app.jsx
@@ -1,6 +1,32 @@
 var App = React.createClass({
   getInitialState: function() {
-    return {songs: this.props.songs, currentSong: ""}
+    return {songs: this.props.songs, currentSong: "", fetched: false}
+  },
+  parseSoundCloud: function(songs) {
+    soundCloudSongs = []
+    var author;
+    var authors = [];
+
+    for (var i = 0; i < songs.length; i++) {
+      if (songs[i].user) {
+        author = songs[i].user.username
+      } else {
+        author = "Unknown Artist"
+      }
+      authors.push(author)
+      soundCloudSongs.push({
+          songName: songs[i].title,
+          songId: songs[i].id,
+          thumbnailLink: songs[i].artwork_url,
+          author: author,
+          source: "soundcloud"
+      });
+      console.log(">>>>>>>>>>>>>>>>>>>>>");
+      console.log(songs[i].id);
+      console.log(">>>>>>>>>>>>>>>>>>>>>");
+    }
+    // console.log(authors.length);
+    return soundCloudSongs;
   },
   searchFilter: function(e) {
     e.preventDefault();
@@ -12,7 +38,16 @@ var App = React.createClass({
       data: {client_id: "4346c8125f4f5c40ad666bacd8e96498", q: $("#search-term").val(), limit: "100", order: "hotness"},
       success: function(data) {
         console.log(data);
-        this.setState({songs: data});
+        console.log(data.length);
+        // this.setState({fetched: true});
+        if(this.state.fetched){
+          this.setState({songs: data.concat(this.parseSoundCloud(data)), fetched: true});
+          console.log(">>>>>>>>>>>>>>>>");
+          console.log(this.state.songs);
+          console.log(">>>>>>>>>>>>>>>>");
+        } else {
+          this.setState({songs: this.parseSoundCloud(data), fetched: true});
+        }
       }.bind(this),
       error: function() {
         this.setState({songs: []});
@@ -25,7 +60,7 @@ var App = React.createClass({
       url: "https://www.googleapis.com/youtube/v3/search",
       data: {part: "id, snippet", q: $("#search-term").val(), key: "AIzaSyCgc_LxS73WKGeyFplInVMxuCR332dbOls"},
       success: function(data) {
-        console.log(data);
+        console.log(data.items);
       },
       error: function() {
         console.log("nope");
