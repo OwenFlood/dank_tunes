afec54e7e153a466bbc96b2c60c2e83dc7aab399
owen flood <oneflood@owens-MacBook-Pro.local>
2016-06-02 09:10:51 -0700

    Add youtube parser

--- app/assets/javascripts/components/song_list.jsx
+++ app/assets/javascripts/components/song_list.jsx
@@ -2,9 +2,7 @@ var SongList = React.createClass({
   render: function() {
     if (this.props.songs.length > 0) {
       var allSongs = this.props.songs.map(function(song, index){
-        console.log(song.songId);
-        console.log("<<<<<<<<<<<<<<<<<<<<<<<<");
-        return <Song class="song-list" playMe={this.props.playMe} imageLink={song.thumbnailLink} songName={song.songName} songId={song.songId} author={song.author} key={index} />
+        return <Song class="song-list" playMe={this.props.playMe} imageLink={song.thumbnailLink} songName={song.songName} songId={song.songId} author={song.author} source={song.source} key={index} />
       }.bind(this));
 
       return <div>{allSongs}</div>
@@ -31,7 +29,7 @@ var Song = React.createClass({
     // var playMe = this.props.playMe.bind(null, {this.props.songName})
     return <div>
               <img src={this.imageFilter()} class="song-thumbnail" />
-              <span className="h4"><a href="#" onClick={() => this.props.playMe(this.props.songName, this.props.songId)}>{this.props.songName}</a></span> - {this.props.author}
+              <span className="h4"><a href="#" onClick={() => this.props.playMe(this.props.songName, this.props.songId)}>{this.props.songName}</a></span> - {this.props.author} Source: {this.props.source}
               <hr />
            </div>
   }
