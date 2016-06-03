cda1882c6ec2f78f51f977a3eff883c6e789685d
Owen Flood <owenhflood@gmail.com>
2016-06-03 13:23:18 -0700

    add embeded youtube component that updates when a youtube video is clicked

---
+++ app/assets/javascripts/components/embed_youtube.jsx
@@ -0,0 +1,10 @@
+var EmbedYoutube = React.createClass({
+  componentDidMount: function() {
+    console.log("Hiiiiii");
+  },
+  render: function() {
+    return <iframe id="player" type="text/html" width="640" height="390"
+           src={"http://www.youtube.com/embed/" + this.props.song[1] + "?enablejsapi=1&origin=http://example.com"}
+           frameborder="0"></iframe>
+  }
+});
