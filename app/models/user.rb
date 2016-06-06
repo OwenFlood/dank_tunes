64da8e1f195a7b1752ac415b18aba18fbb77062b
Owen Flood <owenhflood@gmail.com>
2016-06-06 10:46:25 -0700

    create playlists and users controllers/models slight tweaks to the app.jsx

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX
+end
