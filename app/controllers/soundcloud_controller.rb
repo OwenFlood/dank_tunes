f7a5dafb94eb9a0aeab6579bac8dc4e8ffae4438
Owen Flood <owenhflood@gmail.com>
2016-06-06 15:40:22 -0700

    remove broken links on initial load, switch youtube to load only when clicked, add playlist button

--- app/controllers/soundcloud_controller.rb
+++ app/controllers/soundcloud_controller.rb
@@ -1,8 +1,5 @@
 class SoundcloudController < ApplicationController
   def index
-    client = SoundCloud.new(:client_id => "5d3aaf910add018f35ba65e325fcf227")
-    @tracks = client.get('/tracks', :limit => 10, :order => 'hotness')
-
     if user_signed_in?
       @playlists = Playlist.where(user_id: current_user.id)
     end
