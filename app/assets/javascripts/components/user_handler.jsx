9944ff2b82d40d983f0a9525b29ef02deac275b1
Owen Flood <owenhflood@gmail.com>
2016-06-06 11:15:07 -0700

    add function to stop playing a song, add rack-cors gem

--- app/assets/javascripts/components/user_handler.jsx
+++ app/assets/javascripts/components/user_handler.jsx
@@ -1,9 +1,28 @@
 var UserHandler = React.createClass({
+  logIn: function() {
+
+  },
+  logOut: function() {
+    $.ajax({
+      method: "DELETE",
+      url: "http://localhost:3000/sessions",
+      success: function() {
+        console.log("Successfully logged out");
+      },
+      error: function() {
+        console.log("Unable to log out");
+      }
+    });
+  },
   render: function() {
     if (this.props.user) {
-      return <div>{this.props.user.first_name}</div>
+
+      return  <div>
+                <div>Hello, {this.props.user.first_name}</div>
+                <div><a href="#" onClick={this.logOut}>Log Out</a></div>
+              </div>
     } else {
-      return <div><a href="#" onClick={this.props.logIn}>Log In</a>{this.props.logTest}</div>
+      return <div><a href="#" onClick={this.logIn}>Log In</a>{this.props.logTest}</div>
     }
   }
 });
