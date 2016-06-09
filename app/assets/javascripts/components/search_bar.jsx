ad7e205394b631e55761e2b66cf88cda9269d50b
Owen Flood <owenhflood@gmail.com>
2016-06-08 19:34:09 -0700

    Add bootstrap search bar.

--- app/assets/javascripts/components/search_bar.jsx
+++ app/assets/javascripts/components/search_bar.jsx
@@ -2,7 +2,7 @@ var SearchBar = React.createClass({
   render: function() {
     return <div className="col-md-12 song-search-bar">
              <form className="col-md-3" onSubmit={this.props.onSearch}>
-               <input className="form-control" placeholder="search for a song" id="search-term" type="text" name="music-search-box" />
+               <input className="form-control" placeholder="Search for a song" id="search-term" type="text" name="music-search-box" />
              </form>
            </div>
   }
