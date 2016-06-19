5e504bd9bc20704138426b777534c5f6375efe35
Owen Flood <owenhflood@gmail.com>
2016-06-19 13:30:06 -0700

    fix bug where playlists break without user signed in

--- app/controllers/soundcloud_controller.rb
+++ app/controllers/soundcloud_controller.rb
@@ -2,10 +2,11 @@ class SoundcloudController < ApplicationController
   def index
     if user_signed_in?
       @playlists = current_user.playlists.as_json(include: :playlist_songs)
+    else
+      @playlists = []
     end
   end
 
   def player
-
   end
 end
