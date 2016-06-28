3c7702db7b2157c2c21379e499b12cfe98b05abd
Owen Flood <owenhflood@gmail.com>
2016-06-28 16:55:44 -0700

    add notifier configuration in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,13 @@
 Rails.application.configure do
+
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+  :email => {
+    :deliver_with => :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
+    :email_prefix => "[PREFIX] ",
+    :sender_address => %{"notifier" <error@dank-tunes.com>},
+    :exception_recipients => %w{obeflo@gmail.com}
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
